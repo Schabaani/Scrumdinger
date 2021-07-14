9439f9671ae7d5b49d3a9c999b837411ac41a001
Amir Shabani <a.shabani@shopp.com>
2021-07-14 15:21:25 +0430

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by apple on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
